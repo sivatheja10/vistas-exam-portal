a072ff111155e0b77a244e69190c1f4942a0a812
sivatheja10 <12194495+sivatheja10@users.noreply.github.com>
2020-05-12 08:42:37 +0530

    Inital Setup and Auth

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,     
+  firebaseConfig : {
+    apiKey: "AIzaSyCHNS_kRmi-NUJrB3dznjG904WyVxySwvc",
+    authDomain: "vistasentranceexam.firebaseapp.com",
+    databaseURL: "https://vistasentranceexam.firebaseio.com",
+    projectId: "vistasentranceexam",
+    storageBucket: "vistasentranceexam.appspot.com",
+    messagingSenderId: "329957386251",
+    appId: "1:329957386251:web:db4d981a7674ea6c04bf0e",
+    measurementId: "G-S9LGZXPZF6"
+  }
+};
